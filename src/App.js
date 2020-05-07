0ee6010865e7785c8cec715b9beaf89e4b5aba9b
carole <arbogast.carole@gmail.com>
2020-05-07 17:50:28 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import hordesView from "./images/hordes-main.jpg";
+import styled from "styled-components";
+
+const Core = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+`;
+
+const CoreImage = styled.img`
+	margin: 0 auto 0 auto;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <div>LES DINOS</div> */}
+			<Core>
+				<CoreImage src={hordesView} alt="" />
+			</Core>
+		</div>
+	);
 }
 
 export default App;
